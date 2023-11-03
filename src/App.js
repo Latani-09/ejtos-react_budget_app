f06d509596084188d846b8a59fd8f03e2480663d
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2023-11-03 17:27:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,27 +16,39 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                         <div className='col-sm'>
+                            <Budget/>
+                            <\div>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                        <div className='col-sm'>
+                            < ExpenseList/>
+                            </div>
                         }         
 
                         {
-                            /* Add ExpenseItem component here */
+                             <div className='col-sm'>
+                            <ExpenseItem/>
+                                 </div>
                         }        
 
                         {
-                            /* Add AllocationForm component here under */
+                             <div className='col-sm'>
+                            <AllocationForm/>
+                            </div>
                         }        
 
                 </div>
