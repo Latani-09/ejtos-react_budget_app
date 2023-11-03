cf26603973b87b21015ba61453b7321883adc914
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2023-11-03 22:24:57 +0530

    Update Budget.js      limitations

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,15 +1,28 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import ExpenseTotal from './ExpenseTotal';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        if (newBudget>20000){  
+            alert("The value cannot exceed upper limit of 20000");
+            setNewBudget(20000)
+        }
+        else if (totalExpenses>newBudget){
+            alert("You cannot reduce budget value lower than spending");
+            setNewBudget(totalExpenses)
+        }
+        else{setNewBudget(event.target.value);}        
+        }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget:{budget} £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
