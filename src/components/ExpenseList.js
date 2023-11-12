4e7e81a50cd681b62d849384eb802a672c102ef4
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2023-11-12 08:51:23 +0530

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseList = () => {
+const ExpenseList = (prop) => {
     const { expenses } = useContext(AppContext);
 
     return (
@@ -18,7 +18,7 @@ const ExpenseList = () => {
           </thead>
             <tbody>
             {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
+                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} currency={prop.selectedCurrency} />
             ))}
             </tbody>
         </table>
